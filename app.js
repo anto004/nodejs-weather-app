1744821db36080fb263e09a363a30c979c0222e5
anto004 <anto_004@live.com>
2020-08-17 19:27:57 +0530

    Get location name from command line

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const chalk = require("chalk");
 const geocode = require("./utils/geocode");
 const getForecast = require("./utils/forecast");
 
-geocode("Shillong", (error, weather) => {
+// City name
+const name = process.argv[2];
+if (!name) {
+	return console.log("Please provide a city");
+}
+//Get the weather forecast
+//Pass Geocode coordinates
+// Callback chaining
+geocode(name, (error, weather) => {
 	if (error) {
-		console.log(error);
-	} else {
-		getForecast(weather.latitude, weather.longitude, (error, response) => {
-			try {
-				console.log(response);
-			} catch {
-				console.log(error);
-			}
-		});
+		return console.log(error);
 	}
+
+	getForecast(weather.latitude, weather.longitude, (error, response) => {
+		try {
+			console.log(response);
+		} catch {
+			console.log(error);
+		}
+	});
 });
